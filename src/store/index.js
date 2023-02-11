c6db32dc11614e76f04bfa4dc459b70957681bcf
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-02-11 18:30:31 +0300

    Added storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
-import {combineReducers,createStore} from 'redux'
+import {applyMiddleware,combineReducers,createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
 
 import { accordionReducer } from './accordion-reducer';
+import { booksReducer } from './books-reducer';
 import { burgerReducer } from './burger-reducer'
+import { cardReducer } from './card-reducer';
+import { commentsReducer } from './comments-reducer';
+import { searchReducer } from './search-reducer';
 
 const rootReducer = combineReducers ({
     burger: burgerReducer,
     accordion: accordionReducer,
+    search: searchReducer,
+    card: cardReducer,
+    comments: commentsReducer,
+    books: booksReducer,
 })
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
