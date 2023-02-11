80c0561522ae4f0ceb2beca95275efca652dcaca
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-02-11 17:19:09 +0300

    Add Provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from 'components/layout';
 import { LayoutMain } from 'components/layout-main';
+import { store } from 'store';
 
 import { BookPage } from './pages/book';
 import { MainPage } from './pages/main';
@@ -14,18 +16,20 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
-		<HashRouter>
-			<Routes>
-				<Route path='/' element={<Layout />}>
-					<Route element={<LayoutMain />}>
-						<Route path='/' element={<Navigate to='/books/all' />} />
-						<Route path='/books/:category' element={<MainPage />} />
-						<Route path='/terms' element={<Terms title='Правила пользования' />} />
-						<Route path='/contract' element={<Terms title='Договор оферты' />} />
+		<Provider store = {store}>
+			<HashRouter>
+				<Routes>
+					<Route path='/' element={<Layout />}>
+						<Route element={<LayoutMain />}>
+							<Route path='/' element={<Navigate to='/books/all' />} />
+							<Route path='/books/:category' element={<MainPage />} />
+							<Route path='/terms' element={<Terms title='Правила пользования' />} />
+							<Route path='/contract' element={<Terms title='Договор оферты' />} />
+						</Route>
+						<Route path='/books/:category/:bookId' element={<BookPage />} />
 					</Route>
-					<Route path='/books/:category/:bookId' element={<BookPage />} />
-				</Route>
-			</Routes>
-		</HashRouter>
+				</Routes>
+			</HashRouter>
+		</Provider>
 	</React.StrictMode>
 );
