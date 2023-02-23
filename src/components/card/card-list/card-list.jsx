0daa79f4881573dcbcdf703ba9bbf695ea9b391c
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-02-23 16:37:10 +0300

    Sorting cards by query parameter

--- src/components/card/card-list/card-list.jsx
+++ src/components/card/card-list/card-list.jsx
@@ -3,24 +3,42 @@ import { Rating } from '../rating';
 
 import styles from './card-list.module.css';
 
-export const CardList = ({ book }) => (
-	<div className={styles.card}>
-		<div className={styles.picture}>
-			<img src={book?.image?.url ? `https://strapi.cleverland.by${book.image.url}` : bgDefault} alt='Обложка' />
-		</div>
-		<div className={styles.content}>
-			<h4 className={styles.title}>{book.title}</h4>
-			<p className={styles.author}>
-				{book.authors.map((author) => `${author}, `)} {book.issueYear}
-			</p>
-			<div className={styles.bottom}>
-				<div className={styles.estimation}>
-					<Rating length={Math.round(book.rating) ?? 0} />
+export const CardList = ({ book, query }) => {
+	const selectedWord = () => {
+		if (query) {
+			const regex = new RegExp(`(${query}+)`, 'gi');
+			const parts = book.title.split(regex);
+
+			return parts.map((part) =>
+				regex.test(part) ? <mark  data-test-id='highlight-matches' style={{ color: '#FF5253', background: 'none' }}>{part}</mark> : part
+			);
+		}
+
+		return book.title;
+	};
+
+	const text = selectedWord();
+
+	return (
+		<div className={styles.card}>
+			<div className={styles.picture}>
+				<img src={book?.image?.url ? `https://strapi.cleverland.by${book.image.url}` : bgDefault} alt='Обложка' />
+			</div>
+			<div className={styles.content}>
+				<h4 className={styles.title}>{text}</h4>
+				<p className={styles.author}>
+					{book.authors.map((author) => `${author},`)}
+					{book.issueYear}
+				</p>
+				<div className={styles.bottom}>
+					<div className={styles.estimation}>
+						<Rating length={Math.round(book.rating) ?? 0} />
+					</div>
+					<button className={styles[book.delivery ? 'btn-order' : book.booking ? 'btn-order' : 'btn']} type='button'>
+						{book.delivery ? `Занята до ${book.delivery.dateOrder}` : book.booking ? 'Забронирована' : 'Забронировать'}
+					</button>
 				</div>
-				<button className={styles[book.delivery ? 'btn-order' : book.booking ? 'btn-order' : 'btn']} type='button'>
-					{book.delivery ? `Занята до ${book.delivery.dateOrder}` : book.booking ? 'Забронирована' : 'Забронировать'}
-				</button>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,12 +1,11 @@
 import { CardList } from './card-list';
 import { CardTile } from './card-tile';
 
-
-export const Card = ({ book, groupBy }) => {
+export const Card = ({ book, groupBy, query }) => {
 	switch (groupBy) {
 		case 'groupByList':
-			return <CardList book={book} />;
+			return <CardList book={book} key={groupBy} query={query} />;
 		default:
-			return <CardTile book={book} />;
+			return <CardTile book={book} key={groupBy} query={query} />;
 	}
 };
