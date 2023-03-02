84a358c1318e1537b6dcf074b75b131026e15e3c
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-03-02 21:35:30 +0300

    Аdded authorization routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { SignIn } from 'components/auth/sign-in';
+import { SignUp } from 'components/auth/sign-up';
 import { Layout } from 'components/layout';
 import { LayoutMain } from 'components/layout-main';
+import { AuthPage } from 'pages/auth';
 import { store } from 'store';
 
 import { BookPage } from './pages/book';
@@ -14,19 +17,24 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-	<Provider store = {store}>
-			<HashRouter>
-				<Routes>
-					<Route path='/' element={<Layout />}>
-						<Route element={<LayoutMain />}>
-							<Route path='/' element={<Navigate to='/books/all' />} />
-							<Route path='/books/:category' element={<MainPage />} />
-							<Route path='/terms' element={<Terms title='Правила пользования' />} />
-							<Route path='/contract' element={<Terms title='Договор оферты' />} />
-						</Route>
-						<Route path='/books/:category/:bookId' element={<BookPage />} />
+	<Provider store={store}>
+		<HashRouter>
+			<Routes>
+				<Route element={<AuthPage />}>
+					<Route path='/' element={<Navigate to='/auth' />} />
+					<Route path='/auth' element={<SignIn />} />
+					<Route path='/registration' element={<SignUp />} />
+				</Route>
+				<Route path='/' element={<Layout />}>
+					<Route element={<LayoutMain />}>
+						<Route path='/' element={<Navigate to='/books/all' />} />
+						<Route path='/books/:category' element={<MainPage />} />
+						<Route path='/terms' element={<Terms title='Правила пользования' />} />
+						<Route path='/contract' element={<Terms title='Договор оферты' />} />
 					</Route>
-				</Routes>
-			</HashRouter>
-		</Provider>
+					<Route path='/books/:category/:bookId' element={<BookPage />} />
+				</Route>
+			</Routes>
+		</HashRouter>
+	</Provider>
 );
